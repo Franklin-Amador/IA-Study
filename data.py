fec26719a8c6d101ecee8d8c2926f269452bedb0
Franklin-Amador <joelamador55@gmail.com>
2026-01-27 16:26:56 -0600

    exercise 3 whit some extras

--- data.py
+++ data.py
@@ -3,15 +3,18 @@ import pandas as pl
 import tarfile as tt
 import urllib.request
 
+# Use housing for the first implementation, replace titanic later
+
 def load_house_data():
-    tarball_path = Path("datasets/housting.gz")
+    # tarball_path = Path("datasets/housting.gz")
+    tarball_path = Path("datasets/titanic.tgz")
     if not tarball_path.is_file():
         Path("datasets").mkdir(parents=True, exist_ok=True)
-        url = "https://github.com/ageron/data/raw/main/housing.tgz"
+        url = "https://github.com/ageron/data/raw/main/titanic.tgz"
         urllib.request.urlretrieve(url, tarball_path)
         with tt.open(tarball_path) as housing_tg:
             housing_tg.extractall(path="datasets")
-    return pl.read_csv("datasets/housing/housing.csv")
+    return pl.read_csv("datasets/titanic/test.csv")
 
-housing = load_house_data()
+titanic = load_house_data()
         
